22a9488f47c6899abbcfdd772099d2f221190129
Eliza <elizacc405@gmail.com>
2023-07-19 01:59:09 -0700

    Add new files

---
+++ python-hello_world/4-print_hexa.py
@@ -0,0 +1,2 @@
+for num in range(0, 99):
+    print("{} = 0x{}".format(num, num))
---
+++ python-hello_world/5-print_comb2.py
@@ -0,0 +1,7 @@
+for num in range (0, 100):
+    if num < 10:
+        print("0{},".format(num), end=" ")
+    elif num == 99:
+        print("{}".format(num))
+    else:
+        print("{},".format(num), end=" ")
